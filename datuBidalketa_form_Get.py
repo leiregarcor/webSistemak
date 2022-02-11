1e6eaa630acbc67fcad4842f7c8620af2ef8d59b
leire <leiregarciaupv@gmail.com>
2022-02-11 10:27:06 +0100

    post eta get

---
+++ datuBidalketa_form_Get.py
@@ -0,0 +1,22 @@
+import urllib.parse
+
+import requests
+
+# HELBURUA: NAN zenbakiaren letra kalkulatzeko
+# nabigatzaileak egiten duena simulatuko dugu
+
+uria = "http://ws-sendingformdata.appspot.com/processForm"
+goiburuak = {'Host': 'ws-sendingformdata.appspot.com'}
+edukia = {'nan': '16102927'} # datuak hiztegi batean adieraziko ditut
+# edukia URI-an bertan doa
+edukia_encoded = urllib.parse.urlencode(edukia) #histegi bat ezin da sartu URI-an -> urllib era aproposean jartzen du
+uria = uria + '?' + edukia_encoded
+erantzuna = requests.get(uria, headers=goiburuak, allow_redirects=False)
+
+# HTTP erantzunak:
+kodea = erantzuna.status_code
+deskribapena = erantzuna.reason
+print(str(kodea) + " " + deskribapena)
+
+edukia = erantzuna.content
+print(edukia)
--- datuBidalketa_form_POST.py
+++ datuBidalketa_form_POST.py
@@ -1,4 +1,6 @@
-impor requests
+import urllib.parse
+
+import requests
 
 # HELBURUA: NAN zenbakiaren letra kalkulatzeko
 # nabigatzaileak egiten duena simulatuko dugu
@@ -7,5 +9,17 @@ uria = "http://ws-sendingformdata.appspot.com/processForm"
 goiburuak = {'Host': 'ws-sendingformdata.appspot.com',
              'Content-Type': 'application/x-www-form-urlencoded'}
 
+# datuak hiztegi batean adieraziko ditut
+edukia = {'nan': '16102927'}
+# datuak imprimaki formatuan duen kate batean bihurtuko ditut
+# zelan? urllib liburutegia erabiliz
+edukia_encoded = urllib.parse.urlencode(edukia)
+goiburuak['Content-Length'] = str(len(edukia_encoded))
+erantzuna = requests.post(uria, headers=goiburuak, data=edukia_encoded, allow_redirects=False)
 
-
+# HTTP erantzunak:
+kodea = erantzuna.status_code
+deskribapena = erantzuna.reason
+print(str(kodea) + " " + deskribapena)
+edukia = erantzuna.content
+print(edukia)
